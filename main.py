54c10eb2f187ab8a221aa5d72493c6d3f037ce8e
Anurag Muley <anuragmuley09@gmail.com>
2025-03-04 20:28:59 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 
 app = FastAPI()
 model = joblib.load("model.pkl")
 
-@app.post("/predict")
-def predict(data: list):
-    prediction = model.predict([np.array(data)])
-    return {"prefiction": int(prediction[0])}
+# Create a Pydantic model to define the expected input structure
+class PredictionInput(BaseModel):
+    data: list[float]
 
+@app.post("/predict")
+def predict(input_data: PredictionInput):
+    prediction = model.predict([np.array(input_data.data)])
+    return {"prediction": int(prediction[0])}
